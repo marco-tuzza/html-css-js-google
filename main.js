82272f65e956b6ff339f9a4a7a36a29314390b5f
Marco Tuzza <marcotuzza@hotmail.it>
2020-05-06 15:44:15 +0200

    Esercizio terminato (da fare piccole migliorie)

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+
+var selettore = $(".header-left, main, footer, li:not(.icon-item)")
+
 $(".fa-th").click(showDropdown);
+
+selettore.click(hideDropdown);
 
 function showDropdown () {
     $(".dropdown").toggleClass("visible")
+}
+
+function hideDropdown () {
+    $(".dropdown").removeClass("visible")
 }
